7b3deefddf62a2630c186c4e89e3b22a090e4c2a
PK-Bobale <88286563+PK-Bobale@users.noreply.github.com>
2021-08-02 21:16:31 +0530

    added scripts
    have uploaded c++ and java scripts

---
+++ hackbio_task1_c++script.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+#include<string>
+
+int main ()
+{
+    string name = "Prathamesh K. Bobale";
+    string email = "gmsbin02.prathamesh.bobale@gnkhalsa.edu.in";
+    string username = "@Pratham99";
+    string stack = "Genomics/Data science/ Software/ Transcriptomics";
+
+    cout<<"HackBio Intern Details: \n";
+    cout<<"Name: "<<name<<"\n"<<"Email: "<<email<<"\n"<<"Username: "<<username<<"\n"<<"Biostacks: "<<stack;
+    
+    return 0;
+}
