331657d1d9a27f132727513b2076ed2fc9cb9d22
Kauthar-Omar <kauthyomar@gmail.com>
2021-08-02 18:17:13 +0300

    moved myData.py file to code folder

--- myData.py
+++
@@ -1,8 +0,0 @@
-#project1
-
-myData = { "name": "Abdulafiz",
-           "email": "musaabdulafiz@gmail.com",
-           "slackUsername": "@Abdulafiz",
-           "biostack": "Data Science" }
-
-print(myData["name"], myData["email"], myData["slackUsername"], myData["biostack"])
