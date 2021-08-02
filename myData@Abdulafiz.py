b4815ffc13d948dc91b3430326dab8287fc3099e
Abdulafiz Musa <45498443+a-condor@users.noreply.github.com>
2021-08-02 19:14:37 -0400

    Add files via upload
    updated details

---
+++ myData@Abdulafiz.py
@@ -0,0 +1,8 @@
+#project1
+
+myData = { "name": "Abdulafiz",
+           "email": "musaabdulafiz@gmail.com",
+           "slackUsername": "@Abdulafiz",
+           "biostack": "Data Science" }
+
+print(myData["name"]+","+myData["email"]+","+myData["slackUsername"]+","+myData["biostack"])
