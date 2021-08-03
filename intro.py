e191939b987b3144bbf2ccf6dfdfe1efe28737a0
Ayesha Saman Hawaldar <88281698+asksr9593ars86@users.noreply.github.com>
2021-08-04 00:47:29 +1000

    Update intro.py

--- intro.py
+++ intro.py
@@ -2,4 +2,4 @@ name = "Ayesha Saman Hawaldar"
 email = "ayesh95_saman@yahoo.in"
 slack_username = "@Ayesha"
 biostack = "Vaccine informatics"
-print(name email slack_username biostack sep=',')
+print(name, email, slack_username, biostack, sep=',')
