c6ba8e23f87d59dddf2d14a897c8b1592b6ebd90
kasturipp <88287328+kasturipp@users.noreply.github.com>
2021-08-03 20:46:10 +0530

    introduction file added

---
+++ introductionkasturi.py
@@ -0,0 +1,6 @@
+name = "Kasturi Pradhan"
+email = "kasturi.irl@gmail.com"
+Slack_username = "@Kasturi"
+biostack = "Genomics, Transcriptomics, Data Science, Drug development, Epigenomics, Medical Chemistry and Cheminformatics ,Vaccine informatics, Public health and Genomic Epidemiology"
+
+print ( name , email , Slack_username , biostack , sep = ',' )
