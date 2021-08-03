a869662e2b64957ef7eec5b547e134ed39e684c0
Gracie <58595422+grace190@users.noreply.github.com>
2021-08-03 13:45:00 +0100

    Update Stage_0_grace.py

--- Stage_0_grace.py
+++ Stage_0_grace.py
@@ -2,7 +2,4 @@ name ="Ajayi Grace"
 email = "gracejeta08@gmail.com"
 slack_user_name = "@grace"
 biostack = "Genomics"
-print("NAME: "+name)
-print("EMAIL: "+email) 
-print("SLACK_USERNAME: "+slack_user_name) 
-print("BIOSTACK: "+biostack)
+print("NAME: "+name \n"EMAIL: "+email \n"SLACK_USERNAME: "+slack_user_name) \n"BIOSTACK: "+biostack)
