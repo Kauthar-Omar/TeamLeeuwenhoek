d8e69ad22f6a289f42ea85b3ef00cae8805cbe7e
Abdulafiz Musa <45498443+a-condor@users.noreply.github.com>
2021-08-04 18:10:53 -0400

    myData update
    + twitter handle & hamming distance

--- myData@Abdulafiz.py
+++ myData@Abdulafiz.py
@@ -1,8 +1,25 @@
 #project1
-
 myData = { "name": "Abdulafiz Musa",
            "email": "musaabdulafiz@gmail.com",
            "slackUsername": "@Abdulafiz",
-           "biostack": "Data Science" }
+           "biostack": "Data Science",
+           "twitterHandle": "@Abdulafiz_O" }
 
-print(myData["name"]+","+myData["email"]+","+myData["slackUsername"]+","+myData["biostack"])
+#print(myData["name"]+","+myData["email"]+","+myData["slackUsername"]+","+myData["biostack"])
+
+#Hamming distance
+str1 = myData["slackUsername"]
+str2 = myData["twitter"]
+def hammingDist(str1, str2):
+    i = 0
+    count = 0
+    while (i < len(str1)):
+        if (str1[i] != str2[i]):
+            count += 1
+        i += 1
+    return count
+
+#updated output
+print(myData["name"]+","+myData["email"]+","+myData["slackUsername"]+","+myData["biostack"]+","+
+      myData["twitterHandle"]+","+str(hammingDist(str1, str2)))
+
