75f160f8705de167b5e6d68175134728b88ff869
Kauthar-Omar <kauthyomar@gmail.com>
2021-08-04 18:21:38 +0300

    added twitter handle and hamming distance to the script

--- Kauthar_bashscript.sh
+++ Kauthar_bashscript.sh
@@ -4,5 +4,14 @@ name='Kauthar Omar'
 email='kauthyomar@gmail.com'
 slack_username='@Kauthar'
 biostack='genomics/transcriptomics'
+twitter_handle='@K__Omar'
 
-echo ${name},${email},${slack_username},${biostack}
+for (( i=0; i<${#slack_username}; ++i )); do
+    if [ ${slack_username:$i:1} != ${twitter_handle:$i:1} ]; then
+        let "distance++"
+    fi
+done
+
+hamming_distance=$distance
+
+echo ${name},${email},${slack_username},${biostack},${twitter_handle},${hamming_distance}
