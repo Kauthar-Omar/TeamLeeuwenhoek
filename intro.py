72329e41875163b9e74ddef682b85f71b2c6fee4
Ayesha Saman Hawaldar <88281698+asksr9593ars86@users.noreply.github.com>
2021-08-04 17:45:18 +1000

    Update intro.py

--- intro.py
+++ intro.py
@@ -2,4 +2,6 @@ name = "Ayesha Saman Hawaldar"
 email = "ayesh95_saman@yahoo.in"
 slack_username = "@Ayesha"
 biostack = "Vaccine informatics"
-print(name, email, slack_username, biostack, sep=',')
+twitter_handle = "@Ash95Saman"
+hamming_distance = 
+print(name, email, slack_username, biostack, twitter_handle, hamming_distance, sep=',')
