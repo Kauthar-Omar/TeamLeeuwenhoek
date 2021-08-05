77a378954c11b175c30f5762fe74e98c3148c776
Kauthar-Omar <kauthyomar@gmail.com>
2021-08-05 11:37:59 +0300

    modified script

--- Kauthar_perlscript.pl
+++ Kauthar_perlscript.pl
@@ -5,21 +5,26 @@ $email = 'kauthyomar@gmail.com';
 $slack_username = '@Kauthar';
 $biostack = 'genomics/transcriptomics';
 $twitter_handle = '@K__Omar';
-$humming_distance = hd($slack_username, $twitter_handle);
+$hamming_distance = hd($slack_username, $twitter_handle);
 
 sub hd {
-  #String length is assumed to be equal.
-  #For it to be humming_distance string length must be equal.
   ($k,$l) = @_;
   $len = length ($k);
   $num_mismatch = 0;
 
-  for ($i=0; $i<$len; $i++)
-  {
-   ++$num_mismatch if substr($k, $i, 1) ne substr($l, $i, 1);
+  if ( length($k) ne length($l) ) {
+    print "Strings need to be of equal length to calculate hamming_distance";
+  } else {
+    # Calculating hamming_distance.
+    for ($i=0; $i<$len; $i++)
+    {
+     ++$num_mismatch if substr($k, $i, 1) ne substr($l, $i, 1);
+    }
+
+    return $num_mismatch;
+
   }
 
-  return $num_mismatch;
 }
 
-print "$name,$email,$slack_username,$biostack,$twitter_handle,$humming_distance";
+print "$name,$email,$slack_username,$biostack,$twitter_handle,$hamming_distance";
