c0b711dcb77b9b91203a9911a7856f9da4862ee2
Kauthar-Omar <kauthyomar@gmail.com>
2021-08-05 09:31:47 +0300

    added twitter handle and humming distance

--- Kauthar_perlscript.pl
+++ Kauthar_perlscript.pl
@@ -4,6 +4,22 @@ $name = 'Kauthar Omar';
 $email = 'kauthyomar@gmail.com';
 $slack_username = '@Kauthar';
 $biostack = 'genomics/transcriptomics';
+$twitter_handle = '@K__Omar';
+$humming_distance = hd($slack_username, $twitter_handle);
 
+sub hd {
+  #String length is assumed to be equal.
+  #For it to be humming_distance string length must be equal.
+  ($k,$l) = @_;
+  $len = length ($k);
+  $num_mismatch = 0;
 
-print "$name,$email,$slack_username,$biostack"
+  for ($i=0; $i<$len; $i++)
+  {
+   ++$num_mismatch if substr($k, $i, 1) ne substr($l, $i, 1);
+  }
+
+  return $num_mismatch;
+}
+
+print "$name,$email,$slack_username,$biostack,$twitter_handle,$humming_distance";
