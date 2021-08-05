86b9b915f846ecbf349082758e9450408ee8e9c7
kasturipp <88287328+kasturipp@users.noreply.github.com>
2021-08-05 18:32:22 +0530

    updated the script

---
+++ Kasturi_pythonscript.py.py
@@ -0,0 +1,17 @@
+#task 1
+name = "Kasturi Pradhan"
+email = "kasturi.irl@gmail.com"
+Slack_username = "@Kasturi"
+biostack = "Genomics, Transcriptomics, Data Science, Drug development, Epigenomics, Medical Chemistry and Cheminformatics ,Vaccine informatics, Public health and Genomic Epidemiology"
+twitter_handle = "@kastumuchmess"
+
+#updated task
+
+def hamming_distance(Slack_username, twitter_handle):
+    return sum(c1 !=c2 for c1, c2 in zip(Slack_username, twitter_handle))
+
+
+
+#final output
+print ( name , email , Slack_username , biostack , twitter_handle ,hamming_distance(Slack_username, twitter_handle), sep = ',' )
+
