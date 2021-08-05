254066292dbab3a00e1cca81a61bac2a2b80b365
Kheira Lakhdari <34452306+keirabasma@users.noreply.github.com>
2021-08-05 12:17:00 +0100

    Delete kheira.py

--- kheira.py
+++
@@ -1,6 +0,0 @@
-name= 'Name: Kheira Lakhdari,'
-email= 'Email: kirabasma12@gmail.com,'
-slack= 'Slackid: @Kheira,'
-biostack= 'Biostack: software development / Data science.'
-
-print (name, email, slack, biostack)
