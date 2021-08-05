7d46288674dbb43579c21a14604bd79e46d7abf2
Ayesha Saman Hawaldar <88281698+asksr9593ars86@users.noreply.github.com>
2021-08-05 23:08:28 +1000

    Ayesha_intro

--- intro.py
+++ intro.py
@@ -2,6 +2,17 @@ name = "Ayesha Saman Hawaldar"
 email = "ayesh95_saman@yahoo.in"
 slack_username = "@Ayesha"
 biostack = "Vaccine informatics"
+
+#Updated
+
 twitter_handle = "@Ash95Saman"
-hamming_distance = 
+str1 = "@Ayesha"
+str2 = "@Ash95Saman"
+
+from itertools import zip_longest
+def hamming_dist(str1, str2):
+  return sum(c1 != c2 for c1, c2 in zip_longest(str1, str2))
+
+hamming_dist("@Ayesha", "@Ash95Saman")
+
 print(name, email, slack_username, biostack, twitter_handle, hamming_distance, sep=',')
