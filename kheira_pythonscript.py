65e723936d592c8dd59a0c83738e78195d592b09
Kheira Lakhdari <34452306+keirabasma@users.noreply.github.com>
2021-08-05 15:48:02 +0100

    Add files via upload

---
+++ kheira_pythonscript.py
@@ -0,0 +1,20 @@
+
+import hashlib
+
+def hamming_distance(slack,twitter):
+    return sum(c1 != c2 for c1, c2 in zip(slack, twitter))
+
+
+
+
+
+name= 'Name: Kheira Lakhdari,'
+email= 'Email: kirabasma12@gmail.com,'
+slack= 'Slackid: @Kheira,'
+biostack= 'Biostack: software development / Data science,'
+twitter= 'Twitter: @Kheira,'
+
+
+
+print (name, email, slack, biostack, twitter, hamming_distance(slack,twitter))
+
