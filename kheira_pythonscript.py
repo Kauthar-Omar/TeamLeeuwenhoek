f8a661b177f6fb508c638d79079082a27f709f66
Kheira Lakhdari <34452306+keirabasma@users.noreply.github.com>
2021-08-05 12:17:34 +0100

    Add files via upload

---
+++ kheira_pythonscript.py
@@ -0,0 +1,7 @@
+name= 'Name: Kheira Lakhdari,'
+email= 'Email: kirabasma12@gmail.com,'
+slack= 'Slackid: @Kheira,'
+biostack= 'Biostack: software development / Data science,'
+twitter= 'Twitter: @Kheira'
+
+print (name, email, slack, biostack, twitter)
