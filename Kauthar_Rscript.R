94d49b8162e25e18b671a7ca3f6b1bb19a768b72
Kauthar-Omar <kauthyomar@gmail.com>
2021-08-06 20:07:12 +0300

    added R script

---
+++ Kauthar_Rscript.R
@@ -0,0 +1,20 @@
+#/usr/bin/env Rscript
+
+name <- 'Kauthar Omar'
+email <- 'kauthyomar@gmail.com'
+slack_username <- '@Kauthar'
+biostack <- 'genomics/transcriptomics'
+twitter_username <- '@K__Omar'
+
+#calculating hamming distance.
+s <- c('@', 'K', 'a', 'u', 't', 'h', 'a', 'r')
+t <- c('@', 'K', '_', '_', 'O', 'm', 'a', 'r')
+
+hamming_distance <- sum(s != t)
+
+#Atternatively it can be calculated using DescTools.
+#install.packages("DescTools")
+#library(DescTools)
+#hamming_distance <- StrDist(slack_username, twitter_username, method="hamming")
+
+cat(name, email, slack_username, biostack, twitter_username, hamming_distance, "\n", sep = ',')
