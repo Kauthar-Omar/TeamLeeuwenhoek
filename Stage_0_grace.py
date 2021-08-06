8befb0b0d06b377e07f2e8b0b73099618c63dbe0
Gracie <58595422+grace190@users.noreply.github.com>
2021-08-06 12:44:23 +0100

    Update Stage_0_grace.py

--- Stage_0_grace.py
+++ Stage_0_grace.py
@@ -3,11 +3,6 @@ email = "gracejeta08@gmail.com"
 slack_user_name = "@grace"
 biostack = "Genomics"
 twitter_handle ="Gracy_ldn"
-print("NAME: "+name)
-print("EMAIL: "+email)
-print("SLACK USERNAME: "+slack_user_name)
-print("BIOSTACK: "+biostack)
-print("TWITTER HANDLE: "+twitter_handle)
 def hammingDist(str1, str2):
     i = 0
     count = 0
