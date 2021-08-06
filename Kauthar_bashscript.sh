990c62f8987ea729cb04c1606616645e06a2367e
Kauthar-Omar <kauthyomar@gmail.com>
2021-08-06 03:37:46 +0300

    modified scripts

--- Kauthar_bashscript.sh
+++ Kauthar_bashscript.sh
@@ -6,6 +6,7 @@ slack_username='@Kauthar'
 biostack='genomics/transcriptomics'
 twitter_handle='@K__Omar'
 
+#hamming_distance calculation.
 if [ ${#slack_username} == ${#twitter_handle} ]; then
   for (( i=0; i<${#slack_username}; ++i )); do
     if [ ${slack_username:$i:1} != ${twitter_handle:$i:1} ]; then
@@ -13,7 +14,7 @@ if [ ${#slack_username} == ${#twitter_handle} ]; then
     fi
   done
 else
-  echo "Strings need to be of equal length to calculate humming_distance"
+  echo "Strings need to be of equal length to calculate hamming_distance"
 fi
 
 hamming_distance=$distance
--- Kauthar_perlscript.pl
+++ Kauthar_perlscript.pl
@@ -7,18 +7,19 @@ $biostack = 'genomics/transcriptomics';
 $twitter_handle = '@K__Omar';
 $hamming_distance = hd($slack_username, $twitter_handle);
 
+#hamming_distance function
 sub hd {
-  ($k,$l) = @_;
-  $len = length ($k);
+  ($s,$t) = @_;
+  $len = length ($s);
   $num_mismatch = 0;
 
-  if ( length($k) ne length($l) ) {
+  if ( length($s) ne length($t) ) {
     print "Strings need to be of equal length to calculate hamming_distance";
   } else {
     # Calculating hamming_distance.
     for ($i=0; $i<$len; $i++)
     {
-     ++$num_mismatch if substr($k, $i, 1) ne substr($l, $i, 1);
+     ++$num_mismatch if substr($s, $i, 1) ne substr($t, $i, 1);
     }
 
     return $num_mismatch;
