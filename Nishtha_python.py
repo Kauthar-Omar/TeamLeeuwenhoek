2943cd964b9bf10d1f4ef29239bd9c8654daa23c
nishtha0206 <69421492+nishtha0206@users.noreply.github.com>
2021-08-06 11:50:13 +0530

    Update Nishtha_python.py

--- Nishtha_python.py
+++ Nishtha_python.py
@@ -1,38 +1,14 @@
 #!/usr/bin/env python
-# coding: utf-8
-
-# In[35]:
-
 
 name = ( "Nishtha Malhotra" )
 
-
-# In[36]:
-
-
 email = ( "nishtham17@gmail.com" )
-
-
-# In[37]:
-
 
 slack_username = ( "@Nishtha" )
 
-
-# In[38]:
-
-
 biostack = ( "genomics/transcriptomics/data science/software development/vaccine informatics" )
 
-
-# In[39]:
-
-
 twitter_handle = ( "@nishu_malhotra" )
-
-
-# In[46]:
-
 
 def hamming_distance(slack_username, twitter_handle):
     i = 0
@@ -44,15 +20,4 @@ def hamming_distance(slack_username, twitter_handle):
         i += 1 
     return count
 
-
-# In[47]:
-
-
 print ( name , email , slack_username , biostack , twitter_handle , hamming_distance(slack_username, twitter_handle), sep = ',' )
-
-
-# In[ ]:
-
-
-
-
