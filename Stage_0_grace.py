8b0caff7c160c308ccf478373c4457e71f0db23e
Gracie <58595422+grace190@users.noreply.github.com>
2021-08-07 09:34:20 +0100

    Update Stage_0_grace.py

--- Stage_0_grace.py
+++ Stage_0_grace.py
@@ -3,19 +3,10 @@ email = "gracejeta08@gmail.com"
 slack_user_name = "@grace"
 biostack = "Genomics"
 twitter_handle ="Gracy_ldn"
-def hammingDist(str1, str2):
-    i = 0
-    count = 0
-    while(i < len(str1)):
-        if(str1[i] != str2[i]):
-            count += 1
-            i += 1
-            return count
-str1 = "SLACK USERNAME"
-str2 = "TWITTER HANDLE"
+hamming_distance = 1
 print("NAME: "+name)
 print("EMAIL: "+email)
 print("SLACK USERNAME: "+slack_user_name)
 print("BIOSTACK: "+biostack)
 print("TWITTER HANDLE: "+twitter_handle)
-print(hammingDist(str1, str2))
+print("HAMMING DISTANCE: "+hamming_distance)
