b1999cbc30c0a348407bb6aa0e4ac5f3378d7a4b
Newton2541 <iomolayo2541@gmail.com>
2021-08-07 17:34:16 +0100

    @izic python script

---
+++ izic_pythonscript.py
@@ -0,0 +1,22 @@
+name = 'Isaac Omolayo'
+email = 'iomolayo2541@gmail.com'
+slack_username = '@izic'
+biostack = 'Data analysis/Software Development'
+twitter_handle = '@izic_temi'
+
+def hammingDist(str1, str2):
+    i = 0
+    count = 0
+    if len(str1) == len(str2):
+        while(i < len(str1)):
+            if(str1[i] != str2[i]):
+                count += 1
+            i += 1
+        return count
+    else:
+        return('Strings not of equal length')
+    
+
+hamming_distance = hammingDist(slack_username,twitter_handle)
+print(hamming_distance)
+print(name+","+email+","+slack_username+","+biostack+","+twitter_handle+","+hamming_distance)
