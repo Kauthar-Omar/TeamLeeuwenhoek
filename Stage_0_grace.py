b3ed1db90e9d9f5de502ecf200dadd1f18a615c0
Gracie <58595422+grace190@users.noreply.github.com>
2021-08-07 14:56:27 +0100

    Update Stage_0_grace.py

--- Stage_0_grace.py
+++ Stage_0_grace.py
@@ -1,9 +1,9 @@
- name ="Ajayi Grace"
- email = "gracejeta08@gmail.com"
- slack_user_name = "@Grace"
- biostack = "Genomics"
- twitter_handle ="Gracy_ldn"
- def hammingDist(str1, str2):
+name ="Ajayi Grace"
+email = "gracejeta08@gmail.com"
+slack_user_name = "@Grace"
+biostack = "Genomics"
+twitter_handle ="Gracy_ldn"
+def hammingDist(str1, str2)
      i = 0
      count = 0
      while(i < len(str1)):
@@ -11,6 +11,6 @@
              count += 1
              i += 1
              return count
- str1 = "SLACK USERNAME"
- str2 = "TWITTER HANDLE"
- print(name,email,slack_user_name,biostack,twitter_handle,hammingDist(str1, str2), sep = ',')
+str1 = "SLACK USERNAME"
+str2 = "TWITTER HANDLE"
+print(name\n email\n slack_user_name\n biostack\n twitter_handle\nhammingDist(str1, str2))
